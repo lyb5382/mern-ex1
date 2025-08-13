e313946d8adaeb967a6d2a7ee960dcde1fec9034
lyb5382 <lyb538202@gmail.com>
2025-08-13 11:52:46 +0900

    Update PostForm.jsx

--- frontend/src/components/PostForm.jsx
+++ frontend/src/components/PostForm.jsx
@@ -1,28 +1,31 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const PostForm = () => {
     const API = import.meta.env.VITE_API_URL
     const [posts, setPosts] = useState([])
-    const fetchPosts = async () => {
-        try {
-            const res = await axios.get(`${API}/api/posts`)
-            const data = Array.isArray(res.data) ? res.data : res.data.posts ?? []
-            setPosts(data)
-        } catch (error) {
-            console.log(error, 'failed')
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const res = await axios.get(`${API}/api/posts`)
+                const data = Array.isArray(res.data) ? res.data : res.data.posts ?? []
+                console.log(data)
+                setPosts(data)
+            } catch (error) {
+                console.log(error, 'failed')
+            }
         }
-    }
-    fetchPosts()
+        fetchPosts()
+    }, [])
     return (
         <div>
-            {posts.map((post) => {
+            {posts.map((post) => (
                 <div key={post.id}>
                     {post.title}
                     {post.content}
                     {post.auther}
                 </div>
-            })}
+            ))}
         </div>
     )
 }
